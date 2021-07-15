d88192eb047e8d689ef3a3c1a051a344f524c67a
Ashwin Ramgopal <38460932+ashwin2000@users.noreply.github.com>
2021-07-15 12:40:40 +0530

    Create myGtag.js

---
+++ myGtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XE30J4Q2BH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XE30J4Q2BH');
+</script>
